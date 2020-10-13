63a1c75e9a2c7dea951294127406ceb0ec1ecf6b
monirul-hoque <71548371+monirul-hoque@users.noreply.github.com>
2020-10-13 00:23:50 -0500

    Add files via upload

---
+++ HeyDay.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#User is promped to  type his/her name
+
+echo "Please, enter your name:"
+
+#"read" assigns user input to variable name
+
+read name
+
+#Print statement to user
+
+echo "Hey, $name."
+
